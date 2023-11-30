df88537f3e7d9af97dced7c74a34f0a7e8cc2c3c
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2023-11-30 22:25:45 +0300

    Fixed errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
     result = 0
-    for i in sys.argv:
+    for i in sys.argv[1:]:
         result += int(i)
         print("{}".format(result))
