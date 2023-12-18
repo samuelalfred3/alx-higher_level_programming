fe9952bdd0b93300e1e604c71e5cd713d152eedb
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2023-12-18 15:18:49 +0300

    Fixed some errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -18,16 +18,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
         print("")
-        return ret
-
-    if __name__ == "__main__":
-        my_list = [1, 2, 3, 4, 5]
-
-        nb_print = safe_print_list(my_list, 2)
-        print("nb_print: {:d}".format(nb_print))
-
-        nb_print = safe_print_list(my_list, len(my_list))
-        print("nb_print: {:d}".format(nb_print))
-
-        nb_print = safe_print_list(my_list, len(my_list) + 2)
-        print("nb_print: {:d}".format(nb_print))
+        return (ret)
