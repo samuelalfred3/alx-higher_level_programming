63c0a60df928f3f3d18f4d130f436166f7a5bcde
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2024-01-05 21:18:39 +0300

    Fixed some errors

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,8 +4,3 @@
 class Rectangle:
     """Represent a rectangle."""
     pass
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
