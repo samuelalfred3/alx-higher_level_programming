62ec175a9906fda1fb0b8a263cbbd7a1e72ed41e
samuelalred3 <samuelalfrednjuguna@gmail.com>
2024-02-15 22:53:12 +0300

    Advanced Files

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,6 @@
--- 100-move_to_utf8.sql
+-- Converts the entire database hbtn_0c_0 to UTF8.
 
--- Convert the hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the first_table table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the name field in the first_table table to UTF8
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
