f4ae5704b8d3ef7d2311de5f9d8b1aabd1287b0d
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2024-03-22 14:56:31 +0300

    Corrections

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-import sys
+"""Lists states"""
+
 import MySQLdb
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-        sys.exit(1)
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
